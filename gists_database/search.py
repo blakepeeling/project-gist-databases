d8315a71167016589acf45797c9f64b8f27265a5
EC2 Default User <ec2-user@ip-172-31-5-215.us-west-2.compute.internal>
2019-03-02 02:17:24 +0000

    including fixed requirements file

--- gists_database/search.py
+++ gists_database/search.py
@@ -1,4 +1,17 @@
 from .models import Gist
 
 def search_gists(db_connection, **kwargs):
-    pass
+    
+    if not kwargs:
+        query = 'SELECT * from gists'
+    elif 'github_id' in kwargs:
+        query = 'SELECT * from gists WHERE github_id = :github_id'
+    elif 'created_at' in kwargs:
+        query = 'SELECT * from gists WHERE datetime(created_at) = datetime(:created_at)'
+    
+    cursor = db_connection.execute(query, kwargs)
+    result = []
+    for gist in cursor:
+        result.append(Gist(gist))
+    
+    return result
